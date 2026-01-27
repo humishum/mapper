d51e152d540e1f6f5b894e334847c334c209025b
Humdaan Mustafa <humishum@gmail.com>
2026-01-26 21:56:29 -0800

    savE

--- viewer/backend/config.py
+++ viewer/backend/config.py
@@ -9,7 +9,8 @@ load_dotenv()
 # Data directory containing pointcloud folders
 # DATA_DIR = Path(os.getenv("DATA_DIR", "/home/ape/repos/mapper/data/output_kings_canyon/kings_canyon_must3r_20251229_214719"))
 DATA_DIR = Path("/home/ape/repos/mapper/data/output_kings_canyon/kings_canyon_must3r_20260105_211309")
-DATA_DIR=Path("/home/ape/mapper_output/vggt_trail_test_20260111_013711/outputs")
+DATA_DIR=Path("/home/ape/mapper_output/must3r_011026/kings_canyon_must3r_20260110_202624/outputs/")
+DATA_DIR=Path("/home/ape/mapper_output/must3r_012626/kings_canyon_must3r_20260126_185331/outputs/")
 print(f"DATA_DIR: {DATA_DIR}")
 # Server configuration
 PORT = int(os.getenv("PORT", "8000"))
